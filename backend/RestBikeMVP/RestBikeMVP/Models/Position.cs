c97e61841a5a08a76dccae22cb7d1aedbb8b3ef2
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-11-25 13:37:19 +0100

    feat: add non-present files

---
+++ backend/RestBikeMVP/RestBikeMVP/Models/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace RestBikeMVP.Models
+{
+    internal class Position
+    {
+        [JsonPropertyName("latitude")] public double Latitude { get; set; }
+        [JsonPropertyName("longitude")] public double Longitude { get; set; }
+    }
+}
---
+++ backend/RestBikeMVP/RestBikeMVP/Models/Station.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using RestBikeMVP.Models;
+
+namespace RestBikeMVP
+{
+    internal class Station
+    {
+        [JsonPropertyName("number")] public int Number {  get; set; }
+        [JsonPropertyName("contractName")] public string ContractName { get; set; }
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("address")] public string Address { get; set; }
+        [JsonPropertyName("position")] public Position Position { get; set; }
+        [JsonPropertyName("status")] public string Status { get; set; }
+        [JsonPropertyName("connected")] public Boolean Connected { get; set; }
+    }
+}
