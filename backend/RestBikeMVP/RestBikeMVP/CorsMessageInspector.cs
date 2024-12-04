a8d5f73864d175941411818f80d3c134124fa763
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-12-04 10:22:58 +0100

    feat: self-hosted server

---
+++ backend/RestBikeMVP/RestBikeMVP/CorsMessageInspector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Description;
+
+namespace RestBikeMVP
+{
+    public class CorsMessageInspector : IDispatchMessageInspector
+    {
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            return null; // No action needed here for CORS
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            if (reply.Properties.TryGetValue(HttpResponseMessageProperty.Name, out var httpResponsePropertyObj) &&
+                httpResponsePropertyObj is HttpResponseMessageProperty httpResponseProperty)
+            {
+                httpResponseProperty.Headers["Access-Control-Allow-Origin"] = "*"; // Adjust origin as needed
+                httpResponseProperty.Headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
+                httpResponseProperty.Headers["Access-Control-Allow-Headers"] = "Content-Type, Accept";
+            }
+            else
+            {
+                var httpResponse = new HttpResponseMessageProperty();
+                httpResponse.Headers.Add("Access-Control-Allow-Origin", "*");
+                httpResponse.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                httpResponse.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Accept");
+                reply.Properties.Add(HttpResponseMessageProperty.Name, httpResponse);
+            }
+        }
+    }
+
+    public class CorsBehavior : IServiceBehavior
+    {
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        { 
+
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            foreach (var endpoint in serviceHostBase.ChannelDispatchers)
+            {
+                if (endpoint is ChannelDispatcher channelDispatcher)
+                {
+                    foreach (var endpointDispatcher in channelDispatcher.Endpoints)
+                    {
+                        endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new CorsMessageInspector());
+                    }
+                }
+            }
+        }
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            // No validation needed
+        }
+        
+        public class MyServiceHost : ServiceHost
+        {
+            public MyServiceHost(Type serviceType, params Uri[] baseAddresses) : base(serviceType, baseAddresses)
+            {
+                this.Description.Behaviors.Add(new CorsBehavior());
+            }
+        }
+    }
+}
---
+++ backend/RestBikeMVP/RestBikeMVP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace RestBikeMVP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define the service URI
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/RestBikeMVP/Service1/");
+
+            // Create the ServiceHost
+            using (ServiceHost host = new ServiceHost(typeof(Service1), baseAddress))
+            {
+                
+                    // Enable metadata publishing
+                    ServiceMetadataBehavior smb = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                    if (smb == null)
+                    {
+                        smb = new ServiceMetadataBehavior { HttpGetEnabled = true, HttpsGetEnabled = false };
+                        host.Description.Behaviors.Add(smb);
+                    }
+
+                    // Open the ServiceHost to start listening for messages
+                    host.Open();
+
+                    Console.WriteLine("The service is ready at {0}", baseAddress);
+                    Console.WriteLine("Press <Enter> to stop the service.");
+                    Console.ReadLine();
+            }
+        }
+    }
+}
